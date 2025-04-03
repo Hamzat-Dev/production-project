a2cd797e17c29bad9d4dd4005a0eb19cc4002e02
Hamzat-Dev <abdulla.male@mail.ru>
2025-04-03 17:32:54 +0300

    47.update app roter

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,17 @@
 import { AppRouter } from 'app/providers/router';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { Suspense, useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
-import { userActions } from '../entities/User';
+import { getUserInited, userActions } from '../entities/User';
 import './styles/index.scss';
 
 function App() {
     const { theme } = useTheme();
     const dispatch = useDispatch();
-
+    const inited = useSelector(getUserInited);
     useEffect(() => {
         dispatch(userActions.initAuthData());
     }, [dispatch]);
@@ -23,7 +23,7 @@ function App() {
 
                 <div className="content-page">
                     <Sidebar />
-                    <AppRouter />
+                    {inited && <AppRouter />}
                 </div>
             </Suspense>
         </div>
