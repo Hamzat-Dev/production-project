fcfc888db9e34937b1f15a3ee2de4991e931dd8b
Hamzat-Dev <abdulla.male@mail.ru>
2025-06-09 20:55:57 +0300

    75.migrate to react 18, add mock storybook

--- src/entities/Article/model/services/fetchArticleById/fetchArticleById.ts
+++ src/entities/Article/model/services/fetchArticleById/fetchArticleById.ts
@@ -2,26 +2,33 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import { ThunkConfig } from 'app/providers/StoreProvider/config/StateSchema';
 import { Article } from '../../types/article';
 
-export const fetchArticleById = createAsyncThunk<Article, string, ThunkConfig<string>>(
-    'articleDetails/fetchArticleById',
-    async (articleId, thunkAPI) => {
-        const { extra, rejectWithValue } = thunkAPI;
+export const fetchArticleById = createAsyncThunk<
+Article,
+ string | undefined,
+ ThunkConfig<string>
+ >(
+     'articleDetails/fetchArticleById',
+     async (articleId, thunkAPI) => {
+         const { extra, rejectWithValue } = thunkAPI;
 
-        try {
-            const response = await extra.api.get<Article>(`/articles/${articleId}`, {
-                params: {
-                    _expand: 'user',
-                },
-            });
+         try {
+             if (!articleId) {
+                 throw new Error('Статья не найдена');
+             }
+             const response = await extra.api.get<Article>(`/articles/${articleId}`, {
+                 params: {
+                     _expand: 'user',
+                 },
+             });
 
-            if (!response.data) {
-                throw new Error();
-            }
+             if (!response.data) {
+                 throw new Error();
+             }
 
-            return response.data;
-        } catch (e) {
-            console.log(e);
-            return rejectWithValue('error');
-        }
-    },
-);
+             return response.data;
+         } catch (e) {
+             console.log(e);
+             return rejectWithValue('error');
+         }
+     },
+ );
