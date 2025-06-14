9bf0c58b1aecc44194dc3239364efeebdd4ca0c5
Hamzat-Dev <abdulla.male@mail.ru>
2025-06-14 15:58:08 +0300

    77.add circular dependency plugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,4 +1,5 @@
 import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+import CircularDependencyPlugin from 'circular-dependency-plugin';
 import CopyPlugin from 'copy-webpack-plugin';
 import HTMLWebpackPlugin from 'html-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
@@ -28,7 +29,11 @@ export function buildPlugins({
                 { from: paths.locales, to: paths.buildLocales },
             ],
         }),
-
+        new CircularDependencyPlugin({
+            // exclude detection of files based on a RegExp
+            exclude: /node_modules/,
+            failOnError: true,
+        }),
     ];
 
     if (isDev) {
