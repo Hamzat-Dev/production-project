6b1d34fd421b3ed3af19a432157edc664ccf85a1
Hamzat-Dev <abdulla.male@mail.ru>
2025-07-14 16:00:22 +0300

    96. update eslint plugin my path checker, add auto fix fot public api imports

--- src/features/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/features/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -3,8 +3,8 @@ import DarkIcon from '@/shared/assets/icons/theme-dark.svg';
 import LightIcon from '@/shared/assets/icons/theme-light.svg';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import { Button, ButtonTheme } from '../../../shared/ui/Button/Button';
-import { Theme } from '@/shared/const/theme';
 import { useTheme } from '@/shared/lib/hooks/useTheme/useTheme';
+import { Theme } from '../../../shared/const/theme';
 
 interface ThemeSwitcherProps {
     className?: string;
