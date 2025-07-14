8710fe3b4074e56eba8eafcc34650bf0a02d3c19
Hamzat-Dev <abdulla.male@mail.ru>
2025-07-14 18:58:20 +0300

    101.fix reducers list in DynamicModuleLoader

--- src/shared/ui/Select/Select.tsx
+++ src/shared/ui/Select/Select.tsx
@@ -1,35 +1,35 @@
 import { ChangeEvent, useMemo } from 'react';
-import { useTranslation } from 'react-i18next';
 import { classNames, Mods } from '@/shared/lib/classNames/classNames';
 import cls from './Select.module.scss';
 
-export interface SelectOption <T extends string>{
-    value:T;
-    content:string
+export interface SelectOption<T extends string> {
+    value: T;
+    content: string;
 }
 
 interface SelectProps<T extends string> {
-  className?: string;
-  label?: string;
-  options?:SelectOption<T>[];
-  value?: T;
-  onChange?:(value:T)=>void
-  readonly?: boolean
-
+    className?: string;
+    label?: string;
+    options?: SelectOption<T>[];
+    value?: T;
+    onChange?: (value: T) => void;
+    readonly?: boolean;
 }
 
-export const Select = <T extends string> (props: SelectProps<T>) => {
+export const Select = <T extends string>(props: SelectProps<T>) => {
     const {
         className,
         label,
         options,
-        value,
         onChange,
+        value,
         readonly,
     } = props;
 
-    const onChangeHandler = (e:ChangeEvent<HTMLSelectElement>) => {
-        onChange?.(e.target.value as T);
+    const onChangeHandler = (e: ChangeEvent<HTMLSelectElement>) => {
+        if (onChange) {
+            onChange(e.target.value as T);
+        }
     };
 
     const optionsList = useMemo(() => options?.map((opt) => (
@@ -39,14 +39,10 @@ export const Select = <T extends string> (props: SelectProps<T>) => {
             key={opt.value}
         >
             {opt.content}
-
         </option>
     )), [options]);
 
-    const { t } = useTranslation();
-    const mods: Mods = {
-
-    };
+    const mods: Mods = {};
 
     return (
         <div className={classNames(cls.Wrapper, mods, [className])}>
