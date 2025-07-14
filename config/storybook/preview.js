ccbc3f41acf437436208bc67d047b0061efc5127
Hamzat-Dev <abdulla.male@mail.ru>
2025-07-14 18:38:00 +0300

    99.add addon for storybook

--- config/storybook/preview.js
+++ config/storybook/preview.js
@@ -2,7 +2,6 @@ import { addDecorator } from '@storybook/react';
 import { StyleDecorator } from '../../src/shared/config/storybook/StyleDecorator/StyleDecorator';
 import { ThemeDecorator } from '../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { RouterDecorator } from '../../src/shared/config/storybook/RouterDecorator/RouterDecorator';
-import { StoreDecorator } from '../../src/shared/config/storybook/StoreDecorator/StoreDecorator';
 import {
     SuspenseDecorator,
 } from '../../src/shared/config/storybook/SuspenseDecorator/SuspenseDecorator';
@@ -31,4 +30,3 @@ addDecorator(StyleDecorator);
 addDecorator(ThemeDecorator(Theme.LIGHT));
 addDecorator(RouterDecorator);
 addDecorator(SuspenseDecorator);
-addDecorator(StoreDecorator({}));
